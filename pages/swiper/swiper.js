7a154491155449837b935ebc871edf16abf2e4ef
guowenhuan <guowenhuan_2008@126.com>
2020-11-20 20:08:49 +0800

    swiper

--- pages/swiper/swiper.js
+++ pages/swiper/swiper.js
@@ -4,16 +4,49 @@
  * @Autor: guowenhuan
  * @Date: 2020-11-20 08:38:18
  * @LastEditors: guowenhuan
- * @LastEditTime: 2020-11-20 08:40:59
+ * @LastEditTime: 2020-11-20 13:40:50
  */
 // pages/swiper/swiper.js
+Date.prototype.format = function(fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(
+                RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+
+    return fmt;
+}
+const date = new Date().format("yyyy-MM-dd");
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        flag: false
+        flag: false,
+        date: date,
+        region: ['江苏省', '宿迁市', '沭阳县']
+    },
+
+    regionChange(e) {
+        this.setData({
+            region: e.detail.value
+        });
     },
 
     /**
